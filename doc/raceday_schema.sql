aa24b8ace55fd0d66ae263a2b5ba3ce9772ffb81
ST10492085 <ST10492085@rcconnect.edu.za>
2026-09-04 10:57:27 +0200

    Update raceday_schema.sql

--- doc/raceday_schema.sql
+++ doc/raceday_schema.sql
@@ -5,3 +5,9 @@ empty headers/comments for each table
 CHECK constraints
 UNIQUE constraints
 DEFAULT constraints are present and tidy.
+min 2 organizers
+min 2 participants
+3 events
+categories per event
+sample enrollments
+sample results
